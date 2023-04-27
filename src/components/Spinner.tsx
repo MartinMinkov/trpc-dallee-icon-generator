0a19d8682cff4e695ce91a936015475761e47926
Martin Minkov <minkovlmartin@gmail.com>
2023-04-26 21:31:59 -0700

    fix(Spinner.tsx): remove redundant return statement in Spinner component

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,7 +1,6 @@
 export function Spinner() {
   return (
     <div role="status">
-      return (
       <svg
         aria-hidden="true"
         className="mr-2 h-8 w-8 animate-spin fill-blue-600 text-gray-200 dark:text-gray-600"
