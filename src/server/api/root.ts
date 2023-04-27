8f27d938bc464bdd8e5cc3ad82da03b12e340e20
Martin Minkov <minkovlmartin@gmail.com>
2023-04-26 21:20:30 -0700

    refactor(api): remove exampleRouter and its usage in appRouter
    The exampleRouter has been removed as it is no longer needed. The getAll and getSecretMessage procedures have been moved to other routers.

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,4 @@
 import { createTRPCRouter } from "~/server/api/trpc";
-import { exampleRouter } from "~/server/api/routers/example";
 import { generateRouter } from "~/server/api/routers/generate";
 import { checkoutRouter } from "~/server/api/routers/checkout";
 
@@ -9,7 +8,6 @@ import { checkoutRouter } from "~/server/api/routers/checkout";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
   generate: generateRouter,
   checkout: checkoutRouter,
 });
