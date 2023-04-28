c909bbacc31bb7bf4c458a2ae8961215306b300c
Martin Minkov <minkovlmartin@gmail.com>
2023-04-28 14:28:24 -0700

    refactor(env.mjs): rename AWS environment variables to S3_AWS refactor(generate.ts): use renamed S3_AWS environment variables in S3 configuration

--- src/server/api/routers/generate.ts
+++ src/server/api/routers/generate.ts
@@ -24,10 +24,10 @@ const configuration = new Configuration({
 
 const s3 = new AWS.S3({
   credentials: {
-    accessKeyId: env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: env.S3_AWS_ACCESS_KEY_ID,
+    secretAccessKey: env.S3_AWS_SECRET_ACCESS_KEY,
   },
-  region: env.AWS_REGION,
+  region: env.S3_AWS_REGION,
 });
 
 function generatePrompt(input: GenerateInput) {
