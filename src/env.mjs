bc48f8dafd042f51f0200addc262fb82f2cf9aff
Martin Minkov <minkovlmartin@gmail.com>
2023-04-28 22:52:01 -0700

    fix(env.mjs): change HOSTNAME environment variable name from HOSTNAME to HOST_NAME to match convention

--- src/env.mjs
+++ src/env.mjs
@@ -49,7 +49,7 @@ const client = z.object({
  * @type {Record<keyof z.infer<typeof server> | keyof z.infer<typeof client>, string | undefined>}
  */
 const processEnv = {
-  HOSTNAME: process.env.HOSTNAME,
+  HOSTNAME: process.env.HOST_NAME,
   DATABASE_URL: process.env.DATABASE_URL,
   NODE_ENV: process.env.NODE_ENV,
   NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
