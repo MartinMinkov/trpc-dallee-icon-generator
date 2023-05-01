d16c7cf752f5cf58acffed082d07459674746740
Martin Minkov <minkovlmartin@gmail.com>
2023-04-30 21:46:51 -0700

    feat(community.ts): add randomization to getCommunityIcons query by skipping a random number of icons before selecting 25

--- src/server/api/routers/community.ts
+++ src/server/api/routers/community.ts
@@ -2,7 +2,11 @@ import { createTRPCRouter, publicProcedure } from "../trpc";
 
 export const communityRouter = createTRPCRouter({
   getCommunityIcons: publicProcedure.query(async ({ ctx }) => {
+    const iconsCount = await ctx.prisma.icon.count();
+    const skip = Math.floor(Math.random() * iconsCount);
+
     const icons = await ctx.prisma.icon.findMany({
+      skip,
       take: 25,
       select: {
         id: true,
